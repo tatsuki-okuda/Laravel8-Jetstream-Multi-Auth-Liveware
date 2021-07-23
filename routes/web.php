78dab5e8a1a1c24a64c9308715d4f39a8681365f
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-07-23 13:43:54 +0900

    add admin dashboard & creeate logout

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,9 @@ Route::group([
     Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
 });
 Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->name('dashboard');
+Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
 
 
 // users Route
@@ -37,4 +38,4 @@ Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', f
 Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
     return view('user.index');
 })->name('dashboard');
-Route::get('/user/login', [MainUserController::class, 'Logout'])->name('user.logout');
+Route::get('/user/logout', [MainUserController::class, 'Logout'])->name('user.logout');
